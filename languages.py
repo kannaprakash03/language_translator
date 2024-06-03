724ec7b5ae7e9362fed4064da58d8f482dae5d81
kannaprakash03 <141116577+kannaprakash03@users.noreply.github.com>
2024-06-03 13:26:40 +0530

    Update languages.py

--- languages.py
+++ languages.py
@@ -1,12 +1,16 @@
 languages = [
+    "Arabic",
     "English",
+    "Mandarin Chinese",
     "French",
     "German",
     "Hindi",
     "Japanese",
     "Kannada",
     "Korean",
+    "Russian",
     "Sanskrit",
+    "Spanish",
     "Tamil",
     "Telugu"
 ]
